49bf6281f8875c3388ff901e035c345977bbb921
gamenarkyt <gagaritin11@gmail.com>
2023-11-01 03:30:01 +0300

    add tasks page

---
+++ src/pages/TasksPage/TasksPage.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@/components/ui/Typography/Typography";
+
+import { useTasksStorage } from "@/hooks/useTasksStorage";
+
+export const TasksPage = () => {
+  const { tasks, addTask, deleteTask } = useTasksStorage("tasks", []);
+
+  if (!tasks) return "loading...";
+  return (
+    <div>
+      <Typography>Tasker</Typography>
+      <button onClick={() => addTask(12, "1221")}>add task</button>
+      <button onClick={() => deleteTask(12)}>delete 12 task</button>
+      {tasks.map((task) => {
+        return <div>{task.text}</div>;
+      })}
+    </div>
+  );
+};
