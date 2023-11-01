8a138590194d0d17c53fce32c9c267f69c51b53b
gamenarkyt <gagaritin11@gmail.com>
2023-11-01 03:28:49 +0300

    add typography

---
+++ src/components/ui/Typography/Typography.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import styles from "./Typography.module.css";
+
+import { FC, PropsWithChildren } from "react";
+
+type IVariant = "normal" | "big" | "header";
+interface IProps {
+  className?: string;
+  variant?: IVariant;
+}
+export const Typography: FC<PropsWithChildren<IProps>> = ({
+  children,
+  className,
+  variant = "normal",
+}) => {
+  const _class = clsx(className, styles.typographyroot, {
+    [styles.typography_normal]: variant === "normal",
+    [styles.typography_big]: variant === "big",
+    [styles.typography_header]: variant === "header",
+  });
+
+  return <span className={_class}>{children}</span>;
+};
