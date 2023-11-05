144b52883dc2bc3575b2170e67ad9531243e18bb
gamenarkyt <gagaritin11@gmail.com>
2023-11-05 13:07:35 +0300

    many many changes

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -1,19 +1,21 @@
+import { TasksList } from "@/components/business/TasksList/TasksList";
+import { Fab } from "@/components/ui/Fab/Fab";
 import { Typography } from "@/components/ui/Typography/Typography";
-
 import { useTasksStorage } from "@/hooks/useTasksStorage";
+import dayjs from "dayjs";
 
 export const TasksPage = () => {
-  const { tasks, addTask, deleteTask } = useTasksStorage("tasks", []);
+  const { tasks, addTask } = useTasksStorage();
 
-  if (!tasks) return "loading...";
   return (
     <div>
+      <button onClick={() => console.log(dayjs().locale("ru").format())}>
+        test
+      </button>
+      <button onClick={() => console.log(dayjs().fromNow)}>test2</button>
       <Typography>Tasker test</Typography>
-      <button onClick={() => addTask(12, "1221")}>add task</button>
-      <button onClick={() => deleteTask(12)}>delete 12 task</button>
-      {tasks.map((task) => {
-        return <div>{task.text}</div>;
-      })}
+      <TasksList tasks={tasks} />
+      <Fab onClick={() => addTask("test")} />
     </div>
   );
 };
